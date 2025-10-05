857400fb838bce362556ddad8e1bb40825fdac95
Nairah <nairahthaha@gmail.com>
2025-10-05 19:50:55 +0400

    Added Card.cs to implement card flip rotation
    - Added 3D rotation to flip cards
    - Use color swapping for now (images later)
    - Made Card prefab

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour
+{
+    [SerializeField] private Button cardButton;
+    [SerializeField] private Image cardImage;
+    [SerializeField] private float flipDuration = 0.5f;
+
+    private readonly Color _cardFrontColor = Color.black;
+    private readonly Color _cardBackColor = Color.green;
+    
+    private bool _isFaceUp = false;
+    private bool _isFlipping = false;
+
+    private void OnEnable() => cardButton.onClick.AddListener(OnClickCard);
+    private void OnDisable() => cardButton.onClick.RemoveListener(OnClickCard);
+
+    private void OnClickCard()
+    {
+        FlipCard(true);
+    }
+    
+    private void FlipCard(bool faceUp)
+    {
+        if (_isFlipping) return;
+        if(_isFaceUp == faceUp) return;
+        StartCoroutine(Flip(faceUp));
+    }
+    
+    private IEnumerator Flip(bool faceUp) 
+    {
+        Debug.Log($"Flipping card face up: {faceUp}");
+
+        _isFlipping = true;
+        
+        var timeElapsed = 0f;
+        float startAngle = _isFaceUp ? 180 : 0;
+        float endAngle = faceUp ? 180 : 0;
+        var colorSwapped = false;
+    
+        while (timeElapsed < flipDuration) 
+        {
+            var t = timeElapsed / flipDuration;
+            var angle = Mathf.Lerp(startAngle, endAngle, t);
+            cardImage.transform.rotation = Quaternion.Euler(0, angle, 0);
+    
+            if (t >= 0.5f && !colorSwapped) 
+            {
+                cardImage.color = faceUp ? _cardFrontColor : _cardBackColor;
+                _isFaceUp = faceUp;
+                colorSwapped = true;
+            }
+    
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+        _isFlipping = false;
+    }
+    
+    private void FlipFaceUp() => FlipCard(true);
+    private void FlipFaceDown() => FlipCard(false);
+}
